01f559d75f12f063f5d24c67d49dd08e16cd1660
metaporn.th <metaporn.th@gmail.com>
2026-02-19 19:44:19 +0700

    edit homw page

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-// import './assets/main.css'
 import router from './router'
+import './style.css'
 
 const app =
 createApp(App)
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,39 @@
 <template>
-<div>
-    JOY TO THE WORLD
-</div>
+    <div class="container">
+        <div class="title">
+            WAYFARER
+        </div>
+        <div class="desc">
+            (เวย์-แฟเรอร์) : นักเดินทางที่ชอบสำรวจและค้นหาสิ่งใหม่ๆ อยู่เสมอ
+        </div>
+        <button>GO !</button>
+    </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background:#FFFFFF;
+}
+.title {
+    font-size: 48px;
+    color: #000000;
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.desc {
+    font-size: 24px;
+    color: #000000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
